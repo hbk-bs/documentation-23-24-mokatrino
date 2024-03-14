c59ef0772741d4a9d838e6fdf6ea78ceff1725eb
mokatrino <ka.monkova@hbk-bs.de>
2024-03-14 19:03:11 +0100

    add files
    add html, css files. add license, delete not needed files. add images

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
 	console.log("Hello World");
 });
+
+window.addEventListener('scroll', function () {
+	console.log(window.scrollY);
+	var header = document.querySelector('nav');
+	header.style.opacity = window.scrollY > 0  ? 0 : 1;
+});
